9752bfe99eb580a744cc42f57f605cb9aefa5692
himo05 <112048470+himo05@users.noreply.github.com>
2022-12-07 00:28:48 -0600

    wishlist app - places, visiting places

---
+++ travel_wishlist/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Place
+
+
+class NewPlaceForm(forms.ModelForm):
+    class Meta:
+        model = Place
+        fields = ('name', 'visited')  #want to be displayed
+        
+        
+        
